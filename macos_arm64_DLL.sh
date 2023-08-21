bd4c503cc60b56246dd4860e4b8b51d516b41bd0
johnche <johnche@tencent.com>
2023-08-21 14:44:54 +0800

    update dir for macOS arm64 dylib

--- macos_arm64_DLL.sh
+++ macos_arm64_DLL.sh
@@ -46,8 +46,8 @@ v8_enable_pointer_compression=false
 ninja -C out.gn/arm64.release -t clean
 ninja -C out.gn/arm64.release v8
 
-mkdir -p output/v8/Lib/macOSArm64dylib
-cp out.gn/arm64.release/libv8.dylib output/v8/Lib/macOSArm64dylib/
-cp out.gn/arm64.release/libv8_libplatform.dylib output/v8/Lib/macOSArm64dylib/
-cp out.gn/arm64.release/libv8_libbase.dylib output/v8/Lib/macOSArm64dylib/
-cp out.gn/arm64.release/libchrome_zlib.dylib output/v8/Lib/macOSArm64dylib/
+mkdir -p output/v8/Lib/macOSdylib_arm64
+cp out.gn/arm64.release/libv8.dylib output/v8/Lib/macOSdylib_arm64/
+cp out.gn/arm64.release/libv8_libplatform.dylib output/v8/Lib/macOSdylib_arm64/
+cp out.gn/arm64.release/libv8_libbase.dylib output/v8/Lib/macOSdylib_arm64/
+cp out.gn/arm64.release/libchrome_zlib.dylib output/v8/Lib/macOSdylib_arm64/
