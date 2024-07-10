d019f459b1b13103dc43f9bead59f1fecaca21b7
johnche <johnche@tencent.com>
2024-07-10 11:21:21 +0800

    改回bat，powershell构建dll时会链接失败，原因不明

--- windows_64MD.cmd
+++ windows_64MD.cmd
@@ -1,6 +1,6 @@
 set VERSION=%1
 
-cd %HOMEPATH%
+cd /d %USERPROFILE%
 echo =====[ Getting Depot Tools ]=====
 powershell -command "Invoke-WebRequest https://storage.googleapis.com/chrome-infra/depot_tools.zip -O depot_tools.zip"
 7z x depot_tools.zip -o*
@@ -33,6 +33,12 @@ if "%VERSION%"=="10.6.194" (
     node %~dp0\node-script\do-gitpatch.js -p %GITHUB_WORKSPACE%\patches\win_msvc_v10.6.194.patch
 )
 
+if "%VERSION%"=="11.8.172" (
+    echo =====[ patch 10.6.194 ]=====
+    node %~dp0\node-script\do-gitpatch.js -p %GITHUB_WORKSPACE%\patches\remove_uchar_include_v11.8.172.patch
+    node %~dp0\node-script\do-gitpatch.js -p %GITHUB_WORKSPACE%\patches\win_dll_v11.8.172.patch"
+)
+
 if "%VERSION%"=="9.4.146.24" (
     echo =====[ patch jinja for python3.10+ ]=====
     cd third_party\jinja2
@@ -54,7 +60,7 @@ node %~dp0\node-script\add_arraybuffer_new_without_stl.js .
 node %~dp0\node-script\patchs.js . %VERSION%
 
 echo =====[ Building V8 ]=====
-if "%VERSION%"=="10.6.194" (
+if not "%VERSION%"=="9.4.146.24" (
     call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false v8_static_library=true is_clang=false strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false v8_enable_sandbox=false"
 ) else (
     call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false v8_static_library=true is_clang=false strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false"
