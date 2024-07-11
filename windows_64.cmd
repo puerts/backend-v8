300a4d7eb56c8811b6abee88668c218aa42d9f1b
johnche <johnche@tencent.com>
2024-07-11 11:38:11 +0800

    v8_enable_maglev=false for windows_64.cmd

--- windows_64.cmd
+++ windows_64.cmd
@@ -57,9 +57,15 @@ node %~dp0\node-script\add_arraybuffer_new_without_stl.js .
 node %~dp0\node-script\patchs.js . %VERSION%
 
 echo =====[ Building V8 ]=====
-if not "%VERSION%"=="9.4.146.24" (
+if "%VERSION%"=="11.8.172" (
+    call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false v8_static_library=true is_clang=false strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false v8_enable_sandbox=false v8_enable_maglev=false"
+)
+
+if "%VERSION%"=="10.6.194" (
     call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false v8_static_library=true is_clang=false strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false v8_enable_sandbox=false"
-) else (
+)
+
+if "%VERSION%"=="9.4.146.24" (
     call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false v8_static_library=true is_clang=false strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false"
 )
 call ninja -C out.gn\x64.release -t clean
