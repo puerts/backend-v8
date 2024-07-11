23be43cd887b6ed1da273cc8f433b1de25139ede
johnche <johnche@tencent.com>
2024-07-11 09:16:49 +0800

    关闭v8_monolithic

--- ios_bitcode.sh
+++ ios_bitcode.sh
@@ -29,6 +29,7 @@ node $GITHUB_WORKSPACE/node-script/do-gitpatch.js -p $GITHUB_WORKSPACE/patches/b
 
 if [ "$VERSION" == "11.8.172" ]; then 
   node $GITHUB_WORKSPACE/node-script/do-gitpatch.js -p $GITHUB_WORKSPACE/patches/remove_uchar_include_v11.8.172.patch
+  node $GITHUB_WORKSPACE/node-script/do-gitpatch.js -p $GITHUB_WORKSPACE/patches/enable_wee8_v11.8.172.patch
 fi
 
 echo "=====[ add ArrayBuffer_New_Without_Stl ]====="
@@ -41,18 +42,13 @@ echo "=====[ Building V8 ]====="
 if [ "$VERSION" == "10.6.194" ]; then 
     gn gen out.gn/arm64.release --args="v8_use_external_startup_data=false v8_use_snapshot=true v8_enable_i18n_support=false is_debug=false v8_static_library=true ios_enable_code_signing=false target_os=\"ios\" target_cpu=\"arm64\" v8_enable_pointer_compression=false use_xcode_clang=true enable_ios_bitcode=true symbol_level=0 libcxx_abi_unstable=false v8_enable_sandbox=false use_custom_libcxx=false"
 elif [ "$VERSION" == "11.8.172" ]; then
-    gn gen out.gn/arm64.release --args="v8_use_external_startup_data=false v8_use_snapshot=true v8_enable_i18n_support=false is_debug=false v8_static_library=true ios_enable_code_signing=false target_os=\"ios\" target_cpu=\"arm64\" v8_enable_pointer_compression=false use_xcode_clang=true enable_ios_bitcode=true symbol_level=0 libcxx_abi_unstable=false v8_enable_sandbox=false use_custom_libcxx=false v8_monolithic=true v8_enable_webassembly=false"
+    gn gen out.gn/arm64.release --args="v8_use_external_startup_data=false v8_use_snapshot=true v8_enable_i18n_support=false is_debug=false v8_static_library=true ios_enable_code_signing=false target_os=\"ios\" target_cpu=\"arm64\" v8_enable_pointer_compression=false use_xcode_clang=true enable_ios_bitcode=true symbol_level=0 libcxx_abi_unstable=false v8_enable_sandbox=false use_custom_libcxx=false v8_enable_webassembly=false"
 else
     gn gen out.gn/arm64.release --args="v8_use_external_startup_data=false v8_use_snapshot=true v8_enable_i18n_support=false is_debug=false v8_static_library=true ios_enable_code_signing=false target_os=\"ios\" target_cpu=\"arm64\" v8_enable_pointer_compression=false use_xcode_clang=true enable_ios_bitcode=true symbol_level=0 libcxx_abi_unstable=false"
 fi
 ninja -C out.gn/arm64.release -t clean
 mkdir -p output/v8/Lib/iOS/bitcode
-if [ "$VERSION" == "11.8.172" ]; then 
-  ninja -v -C out.gn/arm64.release v8_monolith
-  cp out.gn/arm64.release/obj/libv8_monolith.a output/v8/Lib/iOS/bitcode/libwee8.a
-else
-  ninja -v -C out.gn/arm64.release wee8
-  cp out.gn/arm64.release/obj/libwee8.a output/v8/Lib/iOS/bitcode/
-fi
+ninja -v -C out.gn/arm64.release wee8
+cp out.gn/arm64.release/obj/libwee8.a output/v8/Lib/iOS/bitcode/
 
 mkdir -p output/v8/Inc/Blob/iOS/bitcode
