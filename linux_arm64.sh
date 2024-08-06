55372845ee7a01edf37d751df4ccbe49dca66d5d
chexiongsheng <johnche@tencent.com>
2024-08-06 23:40:53 +0800

    Update linux_arm64.sh

--- linux_arm64.sh
+++ linux_arm64.sh
@@ -67,6 +67,13 @@ if [ "$VERSION" == "11.8.172" ]; then
   node $GITHUB_WORKSPACE/node-script/do-gitpatch.js -p $GITHUB_WORKSPACE/patches/remove_uchar_include_v11.8.172.patch
 fi
 
+if [ "$NEW_WRAP" == "true" ]; then 
+  echo "=====[ wrap new delete ]====="
+  node $GITHUB_WORKSPACE/node-script/do-gitpatch.js -p $GITHUB_WORKSPACE/patches/wrap_new_delete_v$VERSION.patch
+  sudo apt-get install -y clang
+  llvm-objcopy --version
+fi
+
 echo "=====[ add ArrayBuffer_New_Without_Stl ]====="
 node $GITHUB_WORKSPACE/node-script/add_arraybuffer_new_without_stl.js .
 
@@ -88,6 +95,9 @@ ninja -C out.gn/arm64.release -t clean
 ninja -v -C out.gn/arm64.release wee8
 
 mkdir -p output/v8/Lib/Linux_arm64
+if [ "$NEW_WRAP" == "true" ]; then 
+  llvm-objcopy --redefine-sym=_Znwm=__puerts_wrap__Znwm --redefine-sym=_ZdlPv=__puerts_wrap__ZdlPv --redefine-sym=_Znam=__puerts_wrap__Znam --redefine-sym=_ZdaPv=__puerts_wrap__ZdaPv --redefine-sym=_ZnwmRKSt9nothrow_t=__puerts_wrap__ZnwmRKSt9nothrow_t --redefine-sym=_ZnamRKSt9nothrow_t=__puerts_wrap__ZnamRKSt9nothrow_t out.gn/arm64.release/obj/libwee8.a 
+fi
 cp out.gn/arm64.release/obj/libwee8.a output/v8/Lib/Linux_arm64/
 mkdir -p output/v8/Inc/Blob/Linux_arm64
 
