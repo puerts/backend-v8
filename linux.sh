3ef0e66f7902e3b7f495b51caa94ac90895915e8
johnche <johnche@tencent.com>
2024-08-09 13:34:01 +0800

    之前new和delete的符号不够全

--- linux.sh
+++ linux.sh
@@ -94,7 +94,28 @@ ninja -v -C out.gn/x64.release wee8
 
 mkdir -p output/v8/Lib/Linux
 if [ "$NEW_WRAP" == "with_new_wrap" ]; then 
-  llvm-objcopy --redefine-sym=_Znwm=__puerts_wrap__Znwm --redefine-sym=_ZdlPv=__puerts_wrap__ZdlPv --redefine-sym=_Znam=__puerts_wrap__Znam --redefine-sym=_ZdaPv=__puerts_wrap__ZdaPv --redefine-sym=_ZnwmRKSt9nothrow_t=__puerts_wrap__ZnwmRKSt9nothrow_t --redefine-sym=_ZnamRKSt9nothrow_t=__puerts_wrap__ZnamRKSt9nothrow_t out.gn/x64.release/obj/libwee8.a 
+  llvm-objcopy \
+    --redefine-sym=_Znwm=__puerts_wrap__Znwm \
+    --redefine-sym=_ZdlPv=__puerts_wrap__ZdlPv \
+    --redefine-sym=_Znam=__puerts_wrap__Znam \
+    --redefine-sym=_ZdaPv=__puerts_wrap__ZdaPv \
+    --redefine-sym=_ZnwmRKSt9nothrow_t=__puerts_wrap__ZnwmRKSt9nothrow_t \
+    --redefine-sym=_ZnamRKSt9nothrow_t=__puerts_wrap__ZnamRKSt9nothrow_t \
+    --redefine-sym=_ZdaPvRKSt9nothrow_t=__puerts_wrap__ZdaPvRKSt9nothrow_t \
+    --redefine-sym=_ZdaPvSt11align_val_t=__puerts_wrap__ZdaPvSt11align_val_t \
+    --redefine-sym=_ZdaPvSt11align_val_tRKSt9nothrow_t=__puerts_wrap__ZdaPvSt11align_val_tRKSt9nothrow_t \
+    --redefine-sym=_ZdaPvm=__puerts_wrap__ZdaPvm \
+    --redefine-sym=_ZdaPvmSt11align_val_t=__puerts_wrap__ZdaPvmSt11align_val_t \
+    --redefine-sym=_ZdlPvRKSt9nothrow_t=__puerts_wrap__ZdlPvRKSt9nothrow_t \
+    --redefine-sym=_ZdlPvSt11align_val_t=__puerts_wrap__ZdlPvSt11align_val_t \
+    --redefine-sym=_ZdlPvSt11align_val_tRKSt9nothrow_t=__puerts_wrap__ZdlPvSt11align_val_tRKSt9nothrow_t \
+    --redefine-sym=_ZdlPvm=__puerts_wrap__ZdlPvm \
+    --redefine-sym=_ZdlPvmSt11align_val_t=__puerts_wrap__ZdlPvmSt11align_val_t \
+    --redefine-sym=_ZnamSt11align_val_t=__puerts_wrap__ZnamSt11align_val_t \
+    --redefine-sym=_ZnamSt11align_val_tRKSt9nothrow_t=__puerts_wrap__ZnamSt11align_val_tRKSt9nothrow_t \
+    --redefine-sym=_ZnwmSt11align_val_t=__puerts_wrap__ZnwmSt11align_val_t \
+    --redefine-sym=_ZnwmSt11align_val_tRKSt9nothrow_t=__puerts_wrap__ZnwmSt11align_val_tRKSt9nothrow_t \
+    out.gn/x64.release/obj/libwee8.a 
 fi
 cp out.gn/x64.release/obj/libwee8.a output/v8/Lib/Linux/
 mkdir -p output/v8/Bin/Linux
--- macos_arm64.sh
+++ macos_arm64.sh
@@ -57,7 +57,28 @@ ninja -v -C out.gn/arm64.release wee8
 
 mkdir -p output/v8/Lib/macOS_arm64
 if [ "$NEW_WRAP" == "with_new_wrap" ]; then 
-  llvm-objcopy --redefine-sym=__Znwm=___puerts_wrap__Znwm --redefine-sym=__ZdlPv=___puerts_wrap__ZdlPv --redefine-sym=__Znam=___puerts_wrap__Znam --redefine-sym=__ZdaPv=___puerts_wrap__ZdaPv --redefine-sym=__ZnwmRKSt9nothrow_t=___puerts_wrap__ZnwmRKSt9nothrow_t --redefine-sym=__ZnamRKSt9nothrow_t=___puerts_wrap__ZnamRKSt9nothrow_t out.gn/arm64.release/obj/libwee8.a 
+  llvm-objcopy \
+    --redefine-sym=__Znwm=___puerts_wrap__Znwm \
+    --redefine-sym=__ZdlPv=___puerts_wrap__ZdlPv \
+    --redefine-sym=__Znam=___puerts_wrap__Znam \
+    --redefine-sym=__ZdaPv=___puerts_wrap__ZdaPv \
+    --redefine-sym=__ZnwmRKSt9nothrow_t=___puerts_wrap__ZnwmRKSt9nothrow_t \
+    --redefine-sym=__ZnamRKSt9nothrow_t=___puerts_wrap__ZnamRKSt9nothrow_t \
+    --redefine-sym=__ZdaPvRKSt9nothrow_t=___puerts_wrap__ZdaPvRKSt9nothrow_t \
+    --redefine-sym=__ZdaPvSt11align_val_t=___puerts_wrap__ZdaPvSt11align_val_t \
+    --redefine-sym=__ZdaPvSt11align_val_tRKSt9nothrow_t=___puerts_wrap__ZdaPvSt11align_val_tRKSt9nothrow_t \
+    --redefine-sym=__ZdaPvm=___puerts_wrap__ZdaPvm \
+    --redefine-sym=__ZdaPvmSt11align_val_t=___puerts_wrap__ZdaPvmSt11align_val_t \
+    --redefine-sym=__ZdlPvRKSt9nothrow_t=___puerts_wrap__ZdlPvRKSt9nothrow_t \
+    --redefine-sym=__ZdlPvSt11align_val_t=___puerts_wrap__ZdlPvSt11align_val_t \
+    --redefine-sym=__ZdlPvSt11align_val_tRKSt9nothrow_t=___puerts_wrap__ZdlPvSt11align_val_tRKSt9nothrow_t \
+    --redefine-sym=__ZdlPvm=___puerts_wrap__ZdlPvm \
+    --redefine-sym=__ZdlPvmSt11align_val_t=___puerts_wrap__ZdlPvmSt11align_val_t \
+    --redefine-sym=__ZnamSt11align_val_t=___puerts_wrap__ZnamSt11align_val_t \
+    --redefine-sym=__ZnamSt11align_val_tRKSt9nothrow_t=___puerts_wrap__ZnamSt11align_val_tRKSt9nothrow_t \
+    --redefine-sym=__ZnwmSt11align_val_t=___puerts_wrap__ZnwmSt11align_val_t \
+    --redefine-sym=__ZnwmSt11align_val_tRKSt9nothrow_t=___puerts_wrap__ZnwmSt11align_val_tRKSt9nothrow_t \
+    out.gn/arm64.release/obj/libwee8.a 
 fi
 cp out.gn/arm64.release/obj/libwee8.a output/v8/Lib/macOS_arm64/
 mkdir -p output/v8/Inc/Blob/macOS_arm64
