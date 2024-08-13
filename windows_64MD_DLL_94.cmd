f4c5cd9a15d15a7e450d53513a678eb89a9dab90
johnche <johnche@tencent.com>
2024-08-13 11:54:58 +0800

    新增文件拷贝

--- windows_64MD_DLL_94.cmd
+++ windows_64MD_DLL_94.cmd
@@ -100,9 +100,17 @@ copy /Y out.gn\x64.release\v8_libplatform.dll output\v8\Lib\Win64DLL\
 copy /Y out.gn\x64.release\v8.dll.pdb output\v8\Lib\Win64DLL\
 copy /Y out.gn\x64.release\v8_libbase.dll.pdb output\v8\Lib\Win64DLL\
 copy /Y out.gn\x64.release\v8_libplatform.dll.pdb output\v8\Lib\Win64DLL\
+
+if "%NEW_WRAP%"=="with_new_wrap" (
+  copy /Y out.gn\x64.release\libc++.dll output\v8\Lib\Win64DLL\
+  copy /Y out.gn\x64.release\libc++.dll.pdb output\v8\Lib\Win64DLL\
+)
+
 if "%VERSION%"=="11.8.172" (
   copy /Y out.gn\x64.release\third_party_zlib.dll output\v8\Lib\Win64DLL\
   copy /Y out.gn\x64.release\third_party_zlib.dll.pdb output\v8\Lib\Win64DLL\
+  copy /Y out.gn\x64.release\third_party_abseil-cpp_absl.dll output\v8\Lib\Win64DLL\
+  copy /Y out.gn\x64.release\third_party_abseil-cpp_absl.dll.pdb output\v8\Lib\Win64DLL\
 ) else (
   copy /Y out.gn\x64.release\zlib.dll output\v8\Lib\Win64DLL\
   copy /Y out.gn\x64.release\zlib.dll.pdb output\v8\Lib\Win64DLL\
