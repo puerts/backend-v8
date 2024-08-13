5502117b6999ef9a5c2378fc844ba0b06d970b8b
johnche <johnche@tencent.com>
2024-08-13 16:54:43 +0800

    wrap_symbols.cc修改为放到custom_cxx里

--- windows_64.cmd
+++ windows_64.cmd
@@ -56,7 +56,8 @@ set "CXX_SETTING=is_clang=false use_custom_libcxx=false"
 
 if "%NEW_WRAP%"=="with_new_wrap" (
     echo =====[ wrap new delete ]=====
-    node %~dp0\node-script\do-gitpatch.js -p %GITHUB_WORKSPACE%\patches\wrap_new_delete_v%VERSION%.patch
+    copy /Y %~dp0\node-script\wrap_symbols.cc "third_party\libc++\src\src"
+    node -e "const fs = require('fs'); fs.writeFileSync('buildtools/third_party/libc++/BUILD.gn', fs.readFileSync('buildtools/third_party/libc++/BUILD.gn', 'utf-8').replace('\"//third_party/libc++/src/src/verbose_abort.cpp\",', '\"//third_party/libc++/src/src/verbose_abort.cpp\",\n    \"//third_party/libc++/src/src/wrap_symbols.cc\",'));"
     set "CXX_SETTING=is_clang=true use_custom_libcxx=true"
 )
 
