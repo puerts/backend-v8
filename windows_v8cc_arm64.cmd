b6193e786e4550495995eae742c3d2ac916bf568
Space Time <Zeus6_6@163.com>
2025-07-20 13:26:15 +0800

    还原 vs2019

--- windows_v8cc_arm64.cmd
+++ windows_v8cc_arm64.cmd
@@ -54,10 +54,10 @@ if "%VERSION%"=="9.4.146.24" (
     cd ..\..
 )
 
-@REM echo =====[ add ArrayBuffer_New_Without_Stl ]=====
-@REM node %~dp0\node-script\add_arraybuffer_new_without_stl.js . %VERSION% %NEW_WRAP%
+echo =====[ add ArrayBuffer_New_Without_Stl ]=====
+node %~dp0\node-script\add_arraybuffer_new_without_stl.js . %VERSION% %NEW_WRAP%
 
-@REM node %~dp0\node-script\add_cross_v8cc.js . %VERSION% arm64
+node %~dp0\node-script\add_cross_v8cc.js . %VERSION% arm64
 
 echo =====[ Building V8 ]=====
 if "%VERSION%"=="11.8.172" (
