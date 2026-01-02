d8495520ee384b7c3a21dbe27d0b52c1a841c15b
chexiongsheng <johnche@tencent.com>
2026-01-02 09:27:16 +0800

    use_custom_libcxx=true for windows_64MD_DLL

--- windows_64MD_DLL_94.cmd
+++ windows_64MD_DLL_94.cmd
@@ -84,7 +84,7 @@ if "%VERSION%"=="9.4.146.24" (
 ) else if "%VERSION%"=="11.8.172" (
     call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false %CXX_SETTING% strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false is_component_build=true v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false"
 ) else (
-    call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false is_clang=true use_custom_libcxx=false strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false is_component_build=true v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false"
+    call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false is_clang=true use_custom_libcxx=true strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false is_component_build=true v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false"
 )
 
 call ninja -C out.gn\x64.release -t clean
