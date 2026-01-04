34e30e45ac3592a2ad983dad342ad85b2367c1bf
chexiongsheng <johnche@tencent.com>
2026-01-04 17:26:02 +0800

    ../..\src/execution/frames-inl.h(259,64): error: declaration shadows a variable in namespace 'v8::internal' [-Werror,-Wshadow]

--- windows_v8cc_arm64.cmd
+++ windows_v8cc_arm64.cmd
@@ -54,6 +54,7 @@ if "%VERSION%"=="12.9.202.27" (
 ) else if "%VERSION%"=="13.6.233.17" (
     echo =====[ patch 13.6.233.17 ]=====
     node %~dp0\node-script\do-gitpatch.js -p %GITHUB_WORKSPACE%\patches\enable_wee8_v13.6.233.17.patch
+	node %~dp0\node-script\do-gitpatch.js -p %GITHUB_WORKSPACE%\patches\fp_rename_v13.6.233.17.patch
 )
 
 if "%VERSION%"=="9.4.146.24" (
