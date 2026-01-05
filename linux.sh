21f3f4477bfd34e0881556bdacff00c1efa085c6
chexiongsheng <johnche@tencent.com>
2026-01-05 11:52:58 +0800

    通过patch解决编译错误

--- linux.sh
+++ linux.sh
@@ -70,6 +70,10 @@ if [ "$VERSION" == "12.9.202.27" -o "$VERSION" == "13.6.233.17" ]; then
   node $GITHUB_WORKSPACE/node-script/do-gitpatch.js -p $GITHUB_WORKSPACE/patches/enable_wee8_v$VERSION.patch
 fi
 
+if [ "$VERSION" == "13.6.233.17" ]; then 
+  node $GITHUB_WORKSPACE/node-script/do-gitpatch.js -p $GITHUB_WORKSPACE/patches/double_vector_v13.6.233.17.patch
+fi
+
 CXX_SETTING="use_custom_libcxx=false"
 
 if [ "$NEW_WRAP" == "with_new_wrap" ]; then 
