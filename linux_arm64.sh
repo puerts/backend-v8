7a294b707744de0289b7b6cc02bb982232ce66e9
chexiongsheng <johnche@tencent.com>
2026-01-05 12:18:25 +0800

    unknown type [0x40000014] section `.crel.text' https://groups.google.com/g/v8-users/c/qDJ_XYpig_M/m/qe5XO9PZAwAJ

--- linux_arm64.sh
+++ linux_arm64.sh
@@ -72,6 +72,7 @@ fi
 
 if [ "$VERSION" == "13.6.233.17" ]; then 
   node $GITHUB_WORKSPACE/node-script/do-gitpatch.js -p $GITHUB_WORKSPACE/patches/double_vector_v13.6.233.17.patch
+  node $GITHUB_WORKSPACE/node-script/do-gitpatch.js -p $GITHUB_WORKSPACE/patches/v8_monolithic_for_shared_library_flags_v13.6.233.17.patch
 fi
 
 CXX_SETTING="use_custom_libcxx=false"
@@ -102,7 +103,7 @@ elif [ "$VERSION" == "11.8.172" ]; then
 elif [ "$VERSION" == "12.9.202.27" ]; then
     gn gen out.gn/arm64.release --args="is_debug=false target_cpu=\"arm64\" v8_target_cpu=\"arm64\" v8_enable_i18n_support=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 libcxx_abi_unstable=false v8_enable_pointer_compression=false v8_enable_sandbox=false $CXX_SETTING is_clang=true v8_enable_maglev=false v8_enable_webassembly=false"
 else
-    gn gen out.gn/arm64.release --args="is_debug=false target_cpu=\"arm64\" v8_target_cpu=\"arm64\" v8_enable_i18n_support=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 libcxx_abi_unstable=false v8_enable_pointer_compression=false v8_enable_sandbox=false use_custom_libcxx=false is_clang=true v8_enable_maglev=false v8_enable_webassembly=false"
+    gn gen out.gn/arm64.release --args="is_debug=false target_cpu=\"arm64\" v8_target_cpu=\"arm64\" v8_enable_i18n_support=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 libcxx_abi_unstable=false v8_enable_pointer_compression=false v8_enable_sandbox=false use_custom_libcxx=false is_clang=true v8_enable_maglev=false v8_enable_webassembly=false v8_monolithic=true v8_monolithic_for_shared_library=true"
 fi
 
 ninja -C out.gn/arm64.release -t clean
