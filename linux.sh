9798481203c184530fe1e4357a22bba93ba3e825
chexiongsheng <johnche@tencent.com>
2026-01-05 11:38:01 +0800

    试试linux用较新的系统编译

--- linux.sh
+++ linux.sh
@@ -96,7 +96,7 @@ elif [ "$VERSION" == "11.8.172" ]; then
 elif [ "$VERSION" == "12.9.202.27" ]; then
     gn gen out.gn/x64.release --args="is_debug=false v8_enable_i18n_support=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 libcxx_abi_unstable=false v8_enable_pointer_compression=false v8_enable_sandbox=false $CXX_SETTING is_clang=true v8_enable_maglev=false v8_enable_webassembly=false"
 else
-    gn gen out.gn/x64.release --args="is_debug=false v8_enable_i18n_support=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 libcxx_abi_unstable=false v8_enable_pointer_compression=false v8_enable_sandbox=false use_custom_libcxx=true is_clang=true v8_enable_maglev=false v8_enable_webassembly=false"
+    gn gen out.gn/x64.release --args="is_debug=false v8_enable_i18n_support=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 libcxx_abi_unstable=false v8_enable_pointer_compression=false v8_enable_sandbox=false use_custom_libcxx=false is_clang=true v8_enable_maglev=false v8_enable_webassembly=false"
 fi
 
 ninja -C out.gn/x64.release -t clean
